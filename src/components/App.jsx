4a7467048e5d2e44a6387cd1a6bd296995d5068c
Demian <demyan.korotchenko@gmail.com>
2023-09-21 17:13:50 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,13 +2,6 @@ import { Route, Routes, Navigate } from 'react-router-dom';
 import React, { lazy } from 'react';
 import { Container } from './SharedLayout/SharedLayout.styled';
 
-// import { SharedLayout } from './SharedLayout/SharedLayout';
-// import { HomePage } from './pages/HomePage';
-// import { MoviesPage } from './pages/MoviesPage';
-// import { MovieDetailsPage } from './pages/MovieDetailsPage';
-// import { Cast } from 'components/Cast/Cast';
-// import { Reviews } from 'components/Reviews/Reviews';
-
 const SharedLayout = lazy(() => import('components/SharedLayout/SharedLayout'));
 const HomePage = lazy(() => import('components/pages/HomePage'));
 const MoviesPage = lazy(() => import('components/pages/MoviesPage'));
